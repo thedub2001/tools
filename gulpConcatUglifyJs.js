d7ad2ab3b3c837ef8f31748442fa94ed9f3be99f
thedub2001 <thedub2001@hotmail.com>
2017-03-04 18:26:40 +0100

    Update gulpConcatUglifyJs.js

--- gulpConcatUglifyJs.js
+++ gulpConcatUglifyJs.js
@@ -1,3 +1,5 @@
+// from http://stackoverflow.com/questions/28189177/best-way-to-concat-and-uglify-js-in-gulp
+
 gulp.task('javascript:vendor', function(callback) {
   return gulp.src([
       './node_modules/jquery/dist/jquery.js',
