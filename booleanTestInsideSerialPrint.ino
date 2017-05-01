4bb6baf7bd7992995999ec4fab1beb95f29463c8
thedub2001 <thedub2001@hotmail.com>
2017-05-01 19:22:17 +0200

    Update booleanTestInsideSerialPrint.ino

--- booleanTestInsideSerialPrint.ino
+++ booleanTestInsideSerialPrint.ino
@@ -1,17 +1,12 @@
 boolean test=false;
 
 void setup() { 
-
   Serial.begin(115200);
   Serial.println();
-
 }
 
 void loop() {
-
   test=!test;
-
-  Serial.println(test ? "test=false " : "test=true ");
-
+  Serial.println(test ? "test=true " : "test=false ");
   delay(2000);
 }
